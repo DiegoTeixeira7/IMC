e5bf6202c8ef81e54cdaf17022923d91b7e014c0
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-10-07 15:36:37 -0300

    backend em andamento

--- app/src/main/java/com/example/diegoteixeira/imc/MainActivity.java
+++ app/src/main/java/com/example/diegoteixeira/imc/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.pow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +19,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private String classification(double IMC) {
+        if(IMC < 18.5) {
+            return "Abaixo do Peso";
+        } else if(IMC >= 18.5 && IMC < 25) {
+            return "Saudável";
+        } else if(IMC >= 25 && IMC < 30) {
+            return "Sobrepeso";
+        } else if(IMC >= 30 && IMC < 35) {
+            return "Obesidade Grau I";
+        } else if(IMC >= 35 && IMC < 40) {
+            return "Obesidade Grau II (severa)";
+        } else {
+            return "Obesidade Grau III (mórbida)";
+        }
+    }
+
     public void gerarRelatorio(View view) {
-        Intent it = new Intent(getBaseContext(), ReportNutritional.class);
-        startActivity(it);
+        String nome = ((EditText)findViewById(R.id.name)).getText().toString();
+        int idade = parseInt(((EditText)findViewById(R.id.age)).getText().toString());
+        double peso = parseDouble(((EditText)findViewById(R.id.weight)).getText().toString());
+        double altura = parseDouble(((EditText)findViewById(R.id.height)).getText().toString());
+
+        if(nome.length() == 0) {
+            // exibir toast
+        } else {
+            if(idade < 0 || idade > 140) {
+                //exibir toast
+            }
+
+            if(peso < 1 || peso > 400) {
+                //exibir toast
+            }
+
+            if(altura < 1 || altura > 3) {
+                //exibir toast
+            }
+
+            double IMC = peso/pow(altura,2);
+            String classificacao = classification(IMC);
+
+            Intent it = new Intent(getBaseContext(), ReportNutritional.class);
+
+            it.putExtra("nome", nome);
+            it.putExtra("idade", idade);
+            it.putExtra("peso", peso);
+            it.putExtra("altura", altura);
+            it.putExtra("IMC", IMC);
+            it.putExtra("classificacao", classificacao);
+
+            startActivity(it);
+        }
     }
 }
--- app/src/main/java/com/example/diegoteixeira/imc/ReportNutritional.java
+++ app/src/main/java/com/example/diegoteixeira/imc/ReportNutritional.java
@@ -2,8 +2,10 @@ package com.example.diegoteixeira.imc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ReportNutritional extends AppCompatActivity {
 
@@ -11,6 +13,26 @@ public class ReportNutritional extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_nutritional);
+
+        Intent it = getIntent();
+
+        TextView nome = (TextView)findViewById(R.id.nameText);
+        nome.setText(it.getStringExtra("nome"));
+
+        TextView idade = (TextView)findViewById(R.id.ageText);
+        idade.setText(it.getIntExtra("idade",0)); //arrumar
+
+        TextView peso = (TextView)findViewById(R.id.weightText);
+        peso.setText(it.getIntExtra("peso",0)); // arrumar
+
+        TextView altura = (TextView)findViewById(R.id.heightText);
+        altura.setText(it.getStringExtra("altura"));
+
+        TextView IMC = (TextView)findViewById(R.id.IMCText);
+        IMC.setText(it.getStringExtra("IMC"));
+
+        TextView classificacao = (TextView)findViewById(R.id.reportText);
+        classificacao.setText(it.getStringExtra("classificacao"));
     }
 
     public void voltar(View view) {
